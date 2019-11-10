c6d62a2a78c3c4253cdce94a188f0b9565eba263
luisas <luisa.santus95@gmail.com>
2019-11-10 12:58:57 +0100

    Test KD

--- main.py
+++ main.py
@@ -16,8 +16,8 @@ class Compound:
 
 class Node:
 
-    def __init__(self, compound_index, left_child, right_child):
-        self.compound_index = compound_index
+    def __init__(self, compound_id, left_child, right_child):
+        self.compound_id = compound_id
         self.left_child = left_child
         self.right_child = right_child
 
@@ -29,8 +29,13 @@ class KDtree:
         self.compound_matrix = self.create_compound_matrix(compound_dict)
         self.root = self.build_tree(self.compound_matrix)
 
-    def create_compound_matrix():
-        return
+    def create_compound_matrix(compound_dict):
+        compound_matrix = []
+        for key, value in compound_dict.items():
+            row = value.featureVector
+            row.append(key)
+            compound_matrix.append(row)
+        return compound_matrix
 
     def build_tree(self, compound_matrix, depth=0):
 
@@ -39,7 +44,7 @@ class KDtree:
             return None
 
         # Length of dimensions, equal for all the elements, pick the first one
-        k = len(compound_matrix[0])
+        k = len(compound_matrix[0])-1
         # Check which is the current dimension being considered
         current_dimension = depth % k
         # Sort the elements based on the current_dimension
@@ -48,11 +53,22 @@ class KDtree:
 
         # Create node and construct subtrees
         node = Node()
-        node.compound_index = compound_matrix[median]
+        node.compound_id = compound_matrix[median][-1]
         node.left_child = self.build_tree(compound_matrix[:median], depth + 1)
         node.right_child = self.build_tree(compound_matrix[median + 1:],
                                            depth + 1)
         return node
+
+    def print_tree(self):
+        nodes = [self.root]
+        while len(nodes) != 0:
+            current_node = nodes.pop()
+            print(current_node.compound_id)
+            nodes.append(current_node.left_child)
+            nodes.append(current_node.right_child)
+
+
+
 
     def group(self, x, d, k, *argv):
         return
