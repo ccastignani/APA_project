6a72781a1b2df3b0619562378cac4c859b33a064
Gerard Pradas <gerard.pradas@bsc.es>
2019-11-15 13:59:00 +0100

    comment changed

--- utils.py
+++ utils.py
@@ -1,5 +1,8 @@
 import math
 
 def euclidean_distance(point_x, point_y):
-    """ Method to calculate euclidean distance between two points in n-dimensions """
+    """
+    Method to calculate euclidean distance between two points in n-dimensions.
+    Both points must have the same dimensions 
+    """
     return math.sqrt(sum([(a - b) ** 2 for a, b in zip(point_x, point_y)]))
