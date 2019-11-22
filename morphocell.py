d05ba69b3761f322e7f2ebf84f49a7d875250ff8
luisas <luisa.santus95@gmail.com>
2019-11-22 10:15:17 +0100

    add test matrix

--- morphocell.py
+++ morphocell.py
@@ -75,7 +75,31 @@ if __name__ == '__main__':
     print(" Cell Morphology Analysis Package")
     print("==================================")
     print("\n\n")
+    import numpy as np
+    from morphocell import compound
+    from morphocell import kd_tree
+    id1 = compound.Compound("ID-1", np.array([1,10]))
+    id2 = compound.Compound("ID-2", np.array([1,10]))
+    id3 = compound.Compound("ID-3", np.array([1,20]))
+    id4 = compound.Compound("ID-4", np.array([1,20]))
+    id5 = compound.Compound("ID-5", np.array([1,40]))
 
+    init_dict = {
+        "ID-1": id1,
+        "ID-2": id2,
+        "ID-3": id3,
+        "ID-4": id4,
+        "ID-5": id5
+    }
+    tree = kd_tree.KDTree(init_dict)
+    matrix_non_normalized = tree._KDTree__create_data_matrix(init_dict, False)
+    matrix_normalized = tree._KDTree__create_data_matrix(init_dict, True)
+    assert(float(matrix_non_normalized[0][1]) == 10)
+    assert(float(matrix_normalized[0][1]) == 0.1)
+    assert(float(matrix_non_normalized[0][0]) == 1)
+    assert(float(matrix_normalized[0][0]) == 0.2)
+
+    exit()
     print("===== Reading imaging file =====")
     compounds_dict = Compound.parse_file(imaging_filename,
                                          feature_filename, feature_list)
